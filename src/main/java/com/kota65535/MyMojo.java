d1553e9a5f813281ef96d3e09e221edb69bdbcaa
Tomohiko Ozawa <tomohiko.ozawa@sony.com>
2019-02-10 17:15:52 +0900

    copy javadoc directory

--- src/main/java/com/kota65535/MyMojo.java
+++ src/main/java/com/kota65535/MyMojo.java
@@ -2,52 +2,42 @@ package com.kota65535;
 
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.FileUtils;
 
 /**
  * Goal which touches a timestamp file.
  */
 @Mojo(name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
-public class MyMojo
-    extends AbstractMojo {
+public class MyMojo extends AbstractMojo {
 
-  /**
-   * Location of the file.
-   */
+  @Parameter(property = "javadocDir", required = true)
+  private File javadocDir;
+
+  @Parameter(property = "groovydocDir", required = true)
+  private File groovydocDir;
+
   @Parameter(defaultValue = "${project.build.directory}", property = "outputDir", required = true)
-  private File outputDirectory;
+  private File outputDir;
 
-  public void execute()
-      throws MojoExecutionException {
-    File f = outputDirectory;
+
+  public void execute() throws MojoExecutionException {
+    File f = outputDir;
 
     if (!f.exists()) {
       f.mkdirs();
     }
 
-    File touch = new File(f, "touch.txt");
-
-    FileWriter w = null;
+    // copy all javadoc files
     try {
-      w = new FileWriter(touch);
-
-      w.write("touch.txt");
+      FileUtils.copyDirectory(javadocDir, outputDir);
     } catch (IOException e) {
-      throw new MojoExecutionException("Error creating file " + touch, e);
-    } finally {
-      if (w != null) {
-        try {
-          w.close();
-        } catch (IOException e) {
-          // ignore
-        }
-      }
+      throw new MojoExecutionException("Failed to copy javadoc", e);
     }
   }
 }
