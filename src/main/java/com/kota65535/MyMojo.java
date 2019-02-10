e9e0529593913d923932bb116fb53826b4945c29
Tomohiko Ozawa <tomohiko.ozawa@sony.com>
2019-02-10 17:41:08 +0900

    copy groovydoc html files if not exists

--- src/main/java/com/kota65535/MyMojo.java
+++ src/main/java/com/kota65535/MyMojo.java
@@ -3,6 +3,11 @@ package com.kota65535;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -29,15 +34,44 @@ public class MyMojo extends AbstractMojo {
   public void execute() throws MojoExecutionException {
     File f = outputDir;
 
-    if (!f.exists()) {
-      f.mkdirs();
+    // Initialize outputDir
+    if (f.exists()) {
+      try {
+        FileUtils.deleteDirectory(f);
+
+      } catch(IOException e) {
+        throw new MojoExecutionException("Failed to delete outputDir", e);
+      }
     }
 
-    // copy all javadoc files
+    f.mkdirs();
+
+    // Copy all javadoc files
     try {
-      FileUtils.copyDirectory(javadocDir, outputDir);
+      FileUtils.copyDirectoryStructure(javadocDir, outputDir);
     } catch (IOException e) {
       throw new MojoExecutionException("Failed to copy javadoc", e);
+    }
+
+    // Copy groovydoc file if not exists
+    try {
+      Files.walkFileTree(groovydocDir.toPath(), new SimpleFileVisitor<Path>() {
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+          String strPath = file.toString().replace(groovydocDir.toString(), outputDir.toString());
+          File copyDestFile = new File(strPath);
+          if (!copyDestFile.exists()) {
+            if (!copyDestFile.getParentFile().exists()) {
+              copyDestFile.getParentFile().mkdirs();
+            }
+            FileUtils.copyFile(file.toFile(), copyDestFile);
+            System.out.println(String.format("copied %s -> %s", file.toString(), copyDestFile));
+          }
+          return super.visitFile(file, attrs);
+        }
+      });
+    } catch (IOException e) {
+      throw new MojoExecutionException("Failed to copy groovdoc", e);
     }
   }
 }
